7e2bcc89b2b1458148620f6301fd73ce6559172d
Tigrik121123 <wortexcorporation@gmail.com>
2025-11-05 22:48:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def start(m, res=False):
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-  bot.send_message(m.chat.id, 'Вы написали: ' + message.text)
+  bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
 
 bot.polling(none_stop=True, interval=-0)
